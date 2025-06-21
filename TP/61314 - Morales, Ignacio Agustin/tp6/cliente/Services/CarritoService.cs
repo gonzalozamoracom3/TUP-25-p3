56079262cf83a7bbb6192227642158b1f25799ce
Agustin Morales <agustinmorales1504@gmail.com>
2025-06-20 21:22:02 -0300

    61314-Tp6corregido (#694)
    Corregí la pagina de inicio , el control del stock del carrito , y que imprima un mensaje a la hora de confirmar la compra.

--- TP/61314 - Morales, Ignacio Agustin/tp6/cliente/Services/CarritoService.cs
+++ TP/61314 - Morales, Ignacio Agustin/tp6/cliente/Services/CarritoService.cs
@@ -1,4 +1,7 @@
 using cliente.shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace cliente.Services
 {
@@ -9,9 +12,19 @@ namespace cliente.Services
 
         public IReadOnlyList<ItemCarrito> ObtenerItems() => _items;
 
-        public void AgregarProducto(Producto producto)
+        public event Action<int, int>? CarritoActualizado;
+        public Func<int, int>? ObtenerStockDisponible { get; set; }
 
+        private int StockReal(int productoId)
         {
+            return ObtenerStockDisponible?.Invoke(productoId) ?? 0;
+        }
+
+        public bool AgregarProducto(Producto producto)
+        {
+            if (producto.Stock <= 0)
+                return false;
+
             var itemExistente = _items.FirstOrDefault(i => i.Producto.Id == producto.Id);
             if (itemExistente != null)
             {
@@ -19,21 +32,53 @@ namespace cliente.Services
             }
             else
             {
+           
+                var copiaProducto = new Producto
+                {
+                    Id = producto.Id,
+                    Nombre = producto.Nombre,
+                    Precio = producto.Precio,
+                    ImagenUrl = producto.ImagenUrl,
+                    Descripcion = producto.Descripcion,
+                    Stock = producto.Stock - 1
+                };
+
                 _items.Add(new ItemCarrito
                 {
-                    Producto = producto,
+                    Producto = copiaProducto,
                     Cantidad = 1
                 });
             }
+
+            CarritoActualizado?.Invoke(producto.Id, -1);
+            return true;
         }
+
         public void CambiarCantidad(int productoId, int cambio)
         {
             var item = _items.FirstOrDefault(i => i.Producto.Id == productoId);
-            if (item != null)
+            if (item == null) return;
+
+            int nuevaCantidad = item.Cantidad + cambio;
+
+            if (cambio > 0)
             {
-                item.Cantidad += cambio;
+                int stockDisponible = StockReal(productoId);
+                if (stockDisponible >= cambio)
+                {
+                    item.Cantidad += cambio;
+                    CarritoActualizado?.Invoke(productoId, -cambio);
+                }
+            }
+            else if (cambio < 0)
+            {
+                item.Cantidad = nuevaCantidad;
+                CarritoActualizado?.Invoke(productoId, Math.Abs(cambio));
+
                 if (item.Cantidad <= 0)
+                {
                     _items.Remove(item);
+                }
             }
         }
 
@@ -41,30 +86,38 @@ namespace cliente.Services
         {
             var item = _items.FirstOrDefault(i => i.Producto.Id == productoId);
             if (item != null)
+            {
+                CarritoActualizado?.Invoke(productoId, item.Cantidad);
                 _items.Remove(item);
+            }
         }
 
-        public void Vaciar() => _items.Clear();
+        public void Vaciar()
+        {
+            foreach (var item in _items.ToList())
+            {
+                CarritoActualizado?.Invoke(item.Producto.Id, item.Cantidad);
+            }
+            _items.Clear();
+        }
 
-        public decimal CalcularTotal() => _items.Sum(i => i.Producto.Precio * i.Cantidad);
-
-        public event Action<int>? CarritoActualizado;
+        public decimal CalcularTotal() =>
+            _items.Sum(i => i.Producto.Precio * i.Cantidad);
 
         public void EliminarProducto(int productoId)
         {
             var item = _items.FirstOrDefault(i => i.Producto.Id == productoId);
             if (item != null)
             {
+                CarritoActualizado?.Invoke(productoId, item.Cantidad);
                 _items.Remove(item);
-                CarritoActualizado?.Invoke(productoId);
             }
         }
     }
 
     public class ItemCarrito
     {
-        public Producto Producto { get; set; }
+        public Producto Producto { get; set; } = null!;
         public int Cantidad { get; set; }
     }
 }
-    
